e8be6d5395e51a5954c07df6f200aade4172e226
seulgi <guseul9405@gmail.com>
2023-10-11 23:51:42 +0900

    wxPython 설치 오류 해결

--- expanel.py
+++ expanel.py
@@ -5,6 +5,20 @@ class MainFrame(wx.Frame):
     def __init__(self, parent, id, title):
         wx.Frame.__init__(self, parent, id, title, size=(350, 400))
 
+        panel = wx.Panel(self, -1)
+        panel.SetBackgroundColour('#ccc')
+        vBoxP = wx.BoxSizer(wx.VERTICAL)
+
+        topP = wx.Panel(panel, -1)
+        topP.SetBackgroundColour('#666')
+
+        midP = wx.Panel(panel, -1)
+        bottomP = wx.Panel(panel, -1)
+
+        vBoxP.Add(topP, 1, wx.EXPAND | wx.ALL, 5)
+        vBoxP.Add(midP, 1, wx.EXPAND | wx.ALL, 5)
+        vBoxP.Add(bottomP, 1, wx.EXPAND | wx.ALL, 5)
+
         menu_bar = wx.MenuBar()
         file = wx.Menu()
         edit = wx.Menu()
@@ -14,28 +28,17 @@ class MainFrame(wx.Frame):
         menu_bar.Append(help, '&help')
         self.SetMenuBar(menu_bar)
 
-        # 메뉴바에 해당하는 하위 메뉴들을 아래처럼 설정해줄 수 있다.
         file.Append(101, 'Example menu')
         file.Append(102, 'Exit')
+        help.Append(301, 'Info')
 
-        panel = wx.Panel(self, -1)
-        panel.SetBackgroundColour('#ccc')
-        vBoxP = wx.BoxSizer(wx.VERTICAL)
-
-        topP = wx.Panel(panel, -1)
-        midP = wx.Panel(panel, -1)
-        bottomP = wx.Panel(panel, -1)
-
-        vBoxP.Add(topP, 1, wx.EXPAND | wx.ALL, 5)
-        vBoxP.Add(midP, 1, wx.EXPAND | wx.ALL, 5)
-        vBoxP.Add(bottomP, 1, wx.EXPAND | wx.ALL, 5)
         panel.SetSizer(vBoxP)
         self.Centre()
 
 
 def openFrame():
     app = wx.App()
-    frame = MainFrame(None, -1, 'Example')
+    frame = MainFrame(None, -1, 'Example Framess')
 
     frame.Show()
     app.MainLoop()
