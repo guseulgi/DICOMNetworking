9ccd92949983dfb936e89b590b28f4a583d0400c
seulgi <guseul9405@gmail.com>
2023-10-13 22:49:48 +0900

    Feat: 로고 처리, GridSizer

--- panel/expanel.py
+++ panel/expanel.py
@@ -5,26 +5,50 @@ class MainFrame(wx.Frame):
     def __init__(self, parent, id, title):
         wx.Frame.__init__(self, parent, id, title, size=(350, 400))
 
-        panel = wx.Panel(self, -1)
-        panel.SetBackgroundColour('#ccc')
         vBoxP = wx.BoxSizer(wx.VERTICAL)
 
-        topP = wx.Panel(panel, -1)
-        topP.SetBackgroundColour('#666')
+        # Top Section
+        topP = wx.Panel(self, -1, size=(350, 80))
+        topP.SetBackgroundColour('#fff')
 
-        midP = wx.Panel(panel, -1)
-        bottomP = wx.Panel(panel, -1)
+        grid_top = wx.GridSizer(1, 5, 0, 0)
+
+        self.title_label = wx.StaticText(topP, label="Example\nFrame!")
+        self.title_label.SetFont(wx.Font(17,  wx.DEFAULT, wx.NORMAL, wx.BOLD))
+
+        bitmap = wx.Bitmap('./sources/logo.png')
+        image = bitmap.ConvertToImage()
+        image = image.Scale(32, 32, wx.IMAGE_QUALITY_HIGH)
+        result = wx.Bitmap(image)
+        control = wx.StaticBitmap(topP, -1, result)
+
+        grid_top.AddMany([
+            (wx.Panel(topP, size=(10, 0)), 1, wx.EXPAND | wx.ALL, 5),
+            (self.title_label, 1, wx.EXPAND | wx.ALL, 5),
+            (wx.Panel(topP, size=(30, 0)), 1, wx.EXPAND | wx.ALL, 5),
+            (control, 1, wx.EXPAND | wx.ALL, 5),
+            (wx.Panel(topP, size=(10, 0)), 1, wx.EXPAND | wx.ALL, 5),
+        ])
+
+        topP.SetSizer(grid_top)
+
+        # mid Section
+        midP = wx.Panel(self, -1)
+        bottomP = wx.Panel(self, -1)
 
         vBoxP.Add(topP, 1, wx.EXPAND | wx.ALL, 5)
         vBoxP.Add(midP, 1, wx.EXPAND | wx.ALL, 5)
         vBoxP.Add(bottomP, 1, wx.EXPAND | wx.ALL, 5)
 
+        # 메뉴바
         menu_bar = wx.MenuBar()
         file = wx.Menu()
         edit = wx.Menu()
+        view = wx.Menu()
         help = wx.Menu()
         menu_bar.Append(file, '&File')
         menu_bar.Append(edit, '&Edit')
+        menu_bar.Append(view, '&View')
         menu_bar.Append(help, '&help')
         self.SetMenuBar(menu_bar)
 
@@ -32,7 +56,8 @@ class MainFrame(wx.Frame):
         file.Append(102, 'Exit')
         help.Append(301, 'Info')
 
-        panel.SetSizer(vBoxP)
+        self.SetSizer(vBoxP)
+
         self.Centre()
 
 
